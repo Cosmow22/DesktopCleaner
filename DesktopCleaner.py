bb301dda8d761b3b832bc78c56503e2bdcceb0bf
PC MAISON <cosmowmarchal@gmail.com>
2024-02-11 15:14:15 +0100

    code source en python

---
+++ DesktopCleaner.py
@@ -0,0 +1,26 @@
+import os
+
+def DeleteFolder(Folder):
+    try:
+        os.chdir(Folder)
+        elements = os.listdir() 
+        
+        for element in elements:
+            if os.path.isfile(element):
+                os.remove(element)
+            else:
+                DeleteFolder(element) #si element n'est pas un fichier, c'est un dossier
+
+        os.chdir('..')  # Revenir au répertoire parent
+        os.rmdir(Folder)  # Supprimer le répertoire lui-même
+    except OSError as error:
+        print(f"Erreur lors de la suppression de {Folder}: {error}")
+
+os.chdir(r"C:\Users\Utilisateur\Desktop") #on se place dans le bureau
+elements = os.listdir()
+elements.remove("DesktopCleaner.exe")
+for element in elements: 
+    if os.path.isfile(element):
+        os.remove(element)
+    else:
+        DeleteFolder(element)
